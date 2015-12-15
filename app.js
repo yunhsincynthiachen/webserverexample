0b0e9e31584489e9d9f59d571e9c4a13ff93a38d
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-12-15 18:11:32 -0500

    fixed owner_info syntax

--- app.js
+++ app.js
@@ -598,7 +598,7 @@ app.get('/requests_cars/:borrowerId/:datem/:dated/:datey/:start_time_request/:en
 
       function owner_info(owner) {
         return function doQuery2(cb2) {
-          for (var n=0; n<list_users.length;n++){
+          for (var n=0; n<owner.length;n++){
             CarModel.findOne({'facebook_id' : owner}, function(err3, owner_info){
               if (err3) {
                 res.sendStatus(500);
