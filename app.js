94952eea0ce5e4716f990d7ebacec85a26ee9966
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-12-15 18:40:54 -0500

    moved cb

--- app.js
+++ app.js
@@ -588,7 +588,9 @@ app.get('/requests_cars/:borrowerId/:datem/:dated/:datey/:start_time_request/:en
               }
               // console.log(isAvailable);
               if (isAvailable == 0){
-                getUser(owner, function(user) {});
+                getUser(owner, function(user) { 
+                  // list_users.push(user)
+                });
                 console.log(list_users);
               }
               cb(); 
@@ -601,10 +603,15 @@ app.get('/requests_cars/:borrowerId/:datem/:dated/:datey/:start_time_request/:en
         console.log("here")
         CarModel.findOne({"facebook_id":username}, function(err, user) {
           // console.log(user);
-          list_users.push(user);
-          callback(list_users);
+          pushUserInfo(user);
+          callback(user);
         });
       };
+
+      function pushUserInfo(user) {
+        console.log(user);
+        list_users.push(user);
+      }
 
       // function owner_info(owner) {
       //   return function doQuery2(cb2) {
