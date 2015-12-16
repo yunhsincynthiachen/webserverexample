998dda31b8507518fa4ec02d9f44ee8733a2c9c8
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-12-15 20:14:46 -0500

    fixed missing )

--- app.js
+++ app.js
@@ -584,7 +584,7 @@ app.get('/requests_cars/:borrowerId/:datem/:dated/:datey/:start_time_request/:en
                   else if (parseInt((request[m]["startTime"]).replace(":","")) <= parseInt(end_time_request.replace(":","")) && parseInt(end_time_request.replace(":","")) <= parseInt((request[m]["endTime"]).replace(":",""))){
                     isAvailable = 1;
                   }
-                  else if (parseInt((request[m]["startTime"]).replace(":","")) >= parseInt(start_time_request.replace(":","")) && parseInt(end_time_request.replace(":","") >= parseInt((request[m]["endTime"]).replace(":",""))){
+                  else if (parseInt((request[m]["startTime"]).replace(":","")) >= parseInt(start_time_request.replace(":","")) && parseInt(end_time_request.replace(":","")) >= parseInt((request[m]["endTime"]).replace(":",""))){
                     isAvailable = 1;
                   }
                 }
